3cd43c07400a41717d8ce17f39c728d767db81fc
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-14 18:09:19 +0600

    Add project files.

---
+++ nodeconsoleproject1/app.js
@@ -0,0 +1,9 @@
+"use strict";
+/*console.log('Hello world');*/
+function getSum(first, second) {
+    return first + second;
+}
+console.log(getSum(10, 11));
+console.log(getSum(13, 31));
+console.log(getSum(10.44, 115.11));
+//# sourceMappingURL=app.js.map
